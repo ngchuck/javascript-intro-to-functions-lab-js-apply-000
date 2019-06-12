c3a728d00c9552d2965a55c68b2dd29f3625b184
Deep Chakraborty <deepc.au@gmail.com>
2019-06-12 20:23:28 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(deep)
+{
+  deep=deep.toUpperCase();
+  return deep;
+}
+
+
+function whisper(deep)
+{
+  deep=deep.toLowerCase();
+  return deep;
+}
+
+function logShout(deep)
+{
+  console.log(shout(deep));
+}
